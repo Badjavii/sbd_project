5afa88a2234fbcbd11ae77776e3aa8b70aeee9b5
badjavii <jaotero.24@est.ucab.edu.ve>
2026-06-15 03:37:10 -0400

    feat: add preview of groups with member count to split grupo

--- entrega2_parte2/scripts/menus/clubes/clubes_op8.sql
+++ entrega2_parte2/scripts/menus/clubes/clubes_op8.sql
@@ -1,6 +1,21 @@
 -- ==========================================
 -- ARCHIVO: menu/clubes_op8.sql
 -- ==========================================
+
+prompt ==========================================
+prompt   Grupos con cantidad de miembros activos:
+prompt ==========================================
+select gl.id_club, gl.numero_de_grupo, gl.categoria_edad,
+       count(hg.id_miembro) as total_miembros
+from sojg_grupo_de_lectura gl
+left join sojg_historico_grupo_lectura hg
+    on (gl.id_club = hg.id_club)
+    and (gl.numero_de_grupo = hg.numero_de_grupo)
+    and (hg.fecha_fin is null)
+group by gl.id_club, gl.numero_de_grupo, gl.categoria_edad
+order by gl.id_club, gl.numero_de_grupo;
+prompt ==========================================
+
 accept v_id_club   number prompt 'ID Club: '
 accept v_num_grupo number prompt 'Numero de grupo: '
  
