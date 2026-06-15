42d44b0b19689ad288e181b4b1c17c1747bb0a3f
badjavii <jaotero.24@est.ucab.edu.ve>
2026-06-15 03:37:10 -0400

    feat: add preview of delinquent members to registrar pago

--- entrega2_parte2/scripts/menus/clubes/clubes_op6.sql
+++ entrega2_parte2/scripts/menus/clubes/clubes_op6.sql
@@ -1,6 +1,19 @@
 -- ==========================================
 -- ARCHIVO: menu/clubes_op6.sql
 -- ==========================================
+
+prompt ==========================================
+prompt   Miembros morosos:
+prompt ==========================================
+select l.id_miembro, l.nombre || ' ' || l.apellido as nombre,
+       hm.id_club, c.nombre as club
+from sojg_lector l
+join sojg_historico_membresia hm on (l.id_miembro = hm.id_miembro)
+join sojg_club c on (hm.id_club = c.id_club)
+where (hm.estatus = 'Morosa')
+order by l.id_miembro;
+prompt ==========================================
+
 accept v_id_miembro number prompt 'ID Miembro: '
 accept v_id_club    number prompt 'ID Club: '
 accept v_monto      number prompt 'Monto: '
